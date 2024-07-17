59a0dca1b1e31f1ada62f66da3da59863733125f
LewisTina <aboutou23@outlook.fr>
2024-07-17 09:59:55 +0200

    fix projects list images bug

--- src/components/projects/listing/index.tsx
+++ src/components/projects/listing/index.tsx
@@ -1,26 +1,24 @@
 import projects from '../../../../project.json'
-import ProjectPreview from "@/components/projects/projectPreview"
 import Image from "next/image"
 import { useRouter } from "next/router"
-import SwitchToggle from "@/components/projects/toogle"
 import { useState } from "react"
 import CustomButton from "@/components/button"
 import useTranslation from "next-translate/useTranslation"
 
 export default function Listing(){
-    const finalProject = projects as any
+    const finalProject = projects
+    type projectKey = keyof (typeof projects)
     const {t} = useTranslation('common')
-    const [activeSkills, setActiveSkills] = useState<"app" | "design">("app")
     const router = useRouter()
     const locale = router.locale as "en" | "fr"
 
     return(
         <section className={`flex flex-col items-center justify-start py-20 pb-52`} id="my_project">
             <div className={` flex flex-col justify-start relative items-start max-w-[1535px] w-full rounded-3xl px-16 md:px-4 lg:px-8 text-darkest dark:text-white`}>
-                {/* <SwitchToggle activeSkills={activeSkills} setActiveSkills={setActiveSkills}/> */}
                 <div className={`w-full flex flex-wrap justify-start relative items-start`}>
                     {
-                            Object.keys(projects).map((keyName: any, i) => {
+                            Object.keys(projects).map((_keyName) => {
+                                const keyName = _keyName as projectKey
                                 return (
                                     <>
                                     <div className="w-full h-auto border-2 border-light-grey/50 lg:h-[unset] sm:w-full lg:w-[cal(50%-0.5rem)] relative mt-4 mr-4 sm:mr-0 overflow-hidden cursor-pointer group" onClick={()=>{router.push(`visualisation?id=${keyName}`)}}>
@@ -31,9 +29,7 @@ export default function Listing(){
                                                     src={finalProject[keyName]?.cover?.link}
                                                     alt = {finalProject[keyName]?.cover?.alt}
                                                     width={500}
-                                                    height={500}
-                                                    sizes="100vw"
-                                                    className={"min-h-full h-full w-auto sm:w-full sm:h-auto object-cover object-left"}/>
+                                                    height={500}/>
 
                                                     <div className="z-90 absolute transition-all group-hover:h-full group-hover:opacity-100 opacity-0 h-0 flex items-center justify-center w-full bg-white dark:bg-darkest bottom-0 p-2">
                                                         <h4 className=''>
